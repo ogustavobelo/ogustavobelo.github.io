34b01112c86ab6a6557a81870efae059d4f16365
Gustavo Belo <ogustavobelo@gmail.com>
2026-09-25 20:38:45 -0300

    feat(taxonomy): implement tag links in related posts and add term layout

--- tests/test_rendering.py
+++ tests/test_rendering.py
@@ -44,3 +44,14 @@ def test_hugo_post_page_resolves_local_image_frame_background(tmp_path):
     assert "background-image: url('/posts/2026/01/05/a-cabeca-do-santo/a-cabeca-do-santo.webp')" in post_page
     assert 'data-src="/posts/2026/01/05/a-cabeca-do-santo/a-cabeca-do-santo.webp"' in post_page
     assert "background-image: url('https://bear-images.sfo2.cdn.digitaloceanspaces.com/" not in post_page
+
+
+def test_hugo_tag_links_resolve_to_generated_term_pages(tmp_path):
+    destination = build_site(tmp_path)
+    tag_page = destination / "tags" / "filmes" / "index.html"
+    post_page = (
+        destination / "posts" / "2025" / "03" / "04" / "flow" / "index.html"
+    ).read_text(encoding="utf-8")
+
+    assert tag_page.is_file()
+    assert '<h3><a href="/tags/filmes/">#filmes</a></h3>' in post_page
